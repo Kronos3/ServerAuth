85c6b3127ba378e5c3e97f9e5df499ecc05a2b5c
Kronos3 <dovakhiin1359@gmail.com>
2016-11-30 17:02:04 -0500

    Encrypt to rsa using constant key

---
+++ include/authencrypt.h
@@ -0,0 +1,47 @@
+/*
+ * authencrypt.h
+ * 
+ * Copyright 2016 Andrei Tumbar <atuser@Kronos>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <openssl/rsa.h>
+#include <openssl/engine.h>
+#include <openssl/pem.h>
+#include <base64.h>
+
+#define PADDING RSA_PKCS1_PADDING
+
+const char *b64_pKey;
+const char *b64priv_key;
+
+RSA* loadPUBLICKeyFromString( const char* publicKeyStr );
+RSA* loadPRIVATEKeyFromString( const char* privateKeyStr );
+unsigned char* rsaEncrypt( RSA *pubKey, const unsigned char* str, int dataSize, int *resultLen );
+unsigned char* rsaDecrypt( RSA *privKey, const unsigned char* encryptedData, int *resultLen );
+unsigned char* makeAlphaString( int dataSize );
+char* rsaEncryptThenBase64( RSA *pubKey, unsigned char* binaryData, int binaryDataLen, int *outLen );
+unsigned char* rsaDecryptThisBase64( RSA *privKey, char* base64String, int *outLen );
+char* auth_encrypt (char* asciiData);
+char* auth_decrypt (char* asciiEncrypted);
